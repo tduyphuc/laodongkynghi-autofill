5fe8975a0aad91bc45b580a0f4a31d7179d529ec
Trần Duy Phúc <tduyphuc@gmail.com>
2019-11-09 03:01:59 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,145 @@
-# laodongkynghi-autofill
+# Tự động điền form laodongkynghi Australia
 Chương trình tự động điền form đăng ký ở trang: http://laodongkynghi.dolab.gov.vn
+
+## Mục lục
+
+* [Vài điều về chương trình này](#abouts)
+* [Cài đặt môi trường](#env-setups)
+  * [Phần mềm và thư viện yêu cầu](#prerequisites)
+  * [Cài đặt Python cho Windows](#python-wins)
+  * [Cài đặt thư viện cho Python](#python-libs)
+  * [Tải và cài đặt webdriver](#webdriver)
+    * [Chrome](#webdriver-chrome)
+    * [Firefox](#webdriver-ff)
+* [Chạy chương trình](#usage)
+  * [Điền dữ liệu của vào file Data.xlsx](#fill-data)
+  * [Chạy autofill_script](#run-script)
+* [Notes](#notes)
+
+<a name="abouts"></a>
+
+## Vài điều về chương trình này
+* Đây là chương trình mã nguồn mở, mọi người có thể tự do tải về và chạy theo hướng dẫn mà trong chịu bất kì khoản phí nào.
+* Tác giả chương trình này không chịu trách nhiệm về bất kì trục trặc trong quá trình sử dụng chương trình này. Tuy nhiên, nếu người sử dụng muốn thông báo lỗi hoặc đóng góp cho chương trình thì vui lòng liên hệ với tác giả.
+* Nếu thấy chương trình này hữu ích, các bạn có thể donate cho tác giả :smile: qua:
+    * __PayPal__: https://www.paypal.me/tduyphuc
+    * __VPBank__: 154817174 - Chi nhánh Hồ Chí Minh
+
+<a name="env-setups"></a>
+
+## Cài đặt môi trường
+<a name="prerequisites"></a>
+
+### Phần mềm và thư viện yêu cầu
+* Windows 7 trở lên (ưu tiên win 10) (Đa số các bạn xài Windows nên mình viết chi tiết hướng dẫn chạy trên win, các hệ điều hành khác vẫn chạy được)
+* Python 3+
+* Trình duyệt: Chrome hoặc Firefox (ổn định nhất trên Chrome)
+* [Chrome webdriver](https://chromedriver.chromium.org/downloads) (nếu dùng Chrome)
+* [geckodriver](https://github.com/mozilla/geckodriver/releases) (nếu dùng Firefox)
+* Thư viện Python: selenium, pandas, xlrd
+
+__Lưu ý__: phiên bản driver phải đúng với phiên bản trình duyệt. Ví dụ: Chrome phiên bản 78 thì phải dùng Chrome webdriver 78.
+
+![Chrome version](images/chrome_version.png)
+
+![Chrome webdriver version](images/chrome_wd_version.png)
+
+<a name="python-wins"></a>
+
+### Cài đặt Python cho Windows
+* Vào trang: https://www.python.org/downloads/windows/ để tải Python phiên bản 3 trở lên. Link tải [Python3.6](https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe).
+* Sau khi tải về, chạy file __python-3.6[...].exe__, nhớ tick chọn 2 ô bên dưới !!!:
+
+    ![Python install](images/python_ins.png)
+* Chọn __Install Now__, và đợi Python cài đặt xong:
+
+    ![Python install success](images/python_ins_success.png)
+* Đóng cửa sổ (nhấn __Close__), mở __Command Prompt__ (vào thanh tìm kiếm của window ở góc dưới tìm là thấy). Nhập __"python --version"__, Enter:
+
+    ![Test python](images/test_python.png)
+* Nếu hiện lên phiên bản Python như hình trên là thành công.
+
+<a name="python-libs"></a>
+
+### Cài đặt thư viện cho Python
+* Mở __Command Prompt__ , cài các thư viện cần thiết bằng lệnh:
+    ```
+    pip3 install --upgrade selenium pandas xlrd
+    ```
+    ![Libraries](images/libraries.png)
+* Hiện ra như hình là thành công
+
+<a name="webdriver"></a>
+
+### Tải và cài đặt webdriver
+
+<a name="webdriver-chrome"></a>
+
+#### Chrome
+* Tải Chrome webdriver ở [đây](https://chromedriver.chromium.org/downloads). Lưu ý: phải cùng phiên bản với Chrome đang có ở máy, và nhớ chọn bản cho __windows__.
+* Giải nén file vừa tải về, sẽ được 1 thư mục chứa __chromedriver__.
+* Nhấp chuột phải vào file __chromedriver__ > Properties.
+* Sao chép đường dẫn tới file __chromedriver__ ở LocationLocation
+
+    ![Copy path](images/copy_path.png)
+
+* Ở thanh tìm kiếm của window, tìm "Environment variable". Sẽ thấy khung như hình dưới, chọn __"Environment Variable..."__:
+
+    ![EV Box](images/ev_box.png)
+* Ở khung "System variables", chọn dòng "Path", nhấn "Edit":
+    
+    ![Edit path](images/edit_path.png)
+* Nhấn "New", dán đường dẫn tới webdriver đã sao chép ở trên vào dòng mới, lưu ý phải có dấu "\" ở cuối đường dẫn:
+
+    ![Paste path](images/paste_path.png)
+* OK!
+
+<a name="webdriver-ff"></a>
+
+#### Firefox
+* Cập nhật sau ...
+
+<a name="usage"></a>
+
+## Chạy chương trình
+Để chạy chương trình cần có 2 file __autofill_script.py__ và __Data.xlsx__ được đặt cùng chỗ.
+
+<a name="fill-data"></a>
+
+### Điền dữ liệu của vào file Data.xlsx
+* Điền thông tin cá nhân vào cột "Giá trị" trong file Data.xlsx, các cột khác không được thay đổi
+* Phần nào không cần điền có thể bỏ trống
+
+<a name="run-script"></a>
+
+### Chạy autofill_script
+* Sử dụng mẫu câu lệnh sau:
+    ```
+    python autofill_script.py ^
+    --drivers <Số lượng trình duyệt mở cùng lúc> ^
+    --wait <Thời gian chờ để trang web load (giây)> ^
+    --browser <Loại trình duyệt, mặc định chrome>^
+    --link "<Đường link đến trang có form đăng kí>"
+    ```
+* Ví dụ để chạy __2__ trình duyệt __chrome__ cùng lúc để điền vào trang __https://laodongkynghi.info/dang-ky.html__:
+    ```
+    python autofill_script.py ^
+    --drivers 2 ^
+    --wait 2 ^
+    --browser chrome ^
+    --link "https://laodongkynghi.info/dang-ky.html"
+    ```
+* Mở __Command Prompt__ mới, dán câu lệnh vào, Enter:
+
+    ![Run](images/run.png)
+
+* Trình duyệt sẽ tự động mở lên và chạy, đừng nên tắt trình duyệt khi đang chạy.
+
+<a name="notes"></a>
+
+## Notes
+* Khi chạy thực tế nhớ thay đường dẫn tới trang gốc thay vì trang tập luyện
+* Mở từ 4-5 trình duyệt là ổn
+* Khi xác nhận sẽ có yêu cầu nhập Captcha, version hiện tại chưa hỗ trợ điền Captcha tự động, bạn sẽ phải tự điền và nhấn xác nhận
+* Một số trường sẽ nhập lỗi trong quá trình chạy hoặc không nhập được, vui lòng kiểm tra lại trước khi xác nhận.
+
---
+++ autofill_script.py
@@ -0,0 +1,208 @@
+from selenium import webdriver
+from selenium.webdriver.support import ui
+from selenium.webdriver.common.keys import Keys
+import datetime as dt
+from multiprocessing.pool import ThreadPool
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+import pandas as pd
+import argparse
+
+# LINK = 'http://laodongkynghi.dolab.gov.vn/dang-ky-cap-thu-gioi-thieu/ung-vien/mau-dang-ky'
+FORM_XPATH = "//form"
+
+def page_is_loaded(driver):
+    return driver.find_element_by_xpath(FORM_XPATH) != None
+
+def fill_text_xpath(dn, driver, xpath, txt):
+    try:
+        e = driver.find_element_by_xpath(xpath)
+        e.clear()
+        e.send_keys(txt)
+    except Exception as ex:
+        log("[ERROR][TEXT][%s] %s" % (dn, str(ex)))
+
+def choose_option_click(dn, driver, xpath):
+    try:
+        driver.find_element_by_xpath(xpath).click()
+    except Exception as ex:
+        log("[ERROR][CLICK][%s] %s" % (dn, str(ex)))
+
+def save_screenshot(driver, path):
+    # Ref: https://stackoverflow.com/a/52572919/
+    original_size = driver.get_window_size()
+    required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+    required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+    driver.set_window_size(required_width, required_height)
+    # driver.save_screenshot(path)  # has scrollbar
+    driver.find_element_by_xpath(FORM_XPATH).screenshot(path)  # avoids scrollbar
+    driver.set_window_size(original_size['width'], original_size['height'])
+
+def log(txt):
+    timestamp_now = dt.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    print("[%s]%s" % (timestamp_now, txt))
+
+
+xpath_config = {
+    "FullName": "(//form/div[@class=\"form-group\"][1]//input)[1]",
+    "DOB": "(//form/div[@class=\"form-group\"][1]//input)[2]",
+    "Sex": "(//form/div[@class=\"form-group\"][1]//select)[1]/option[{}]",
+    "Email": "(//form/div[@class=\"form-group\"][2]//input)[1]",
+    "Phone": "(//form/div[@class=\"form-group\"][2]//input)[2]",
+    "Marriage": "(//form/div[@class=\"form-group\"][2]//select)[1]/option[{}]",
+    "Address": "(//form/div[@class=\"form-group\"][3]//input)[1]",
+    "Province": "(//form/div[@class=\"form-group\"][4]//select)[1]/option[contains(.,'{}')]",
+    "District": "(//form/div[@class=\"form-group\"][4]//select)[2]/option[contains(.,'{}')]",
+    "Town": "(//form/div[@class=\"form-group\"][4]//input)[1]",
+    "CurrentAddress": "(//form/div[@class=\"form-group\"][5]//input)[1]",
+    "PassportNum": "(//form/div[@class=\"form-group\"][6]//input)[1]",
+    "PassportPlace": "(//form/div[@class=\"form-group\"][6]//input)[2]",
+    "PassportFromDate": "(//form/div[@class=\"form-group\"][6]//input)[3]",
+    "PassportToDate": "(//form/div[@class=\"form-group\"][6]//input)[4]",
+    "IsGraduate": "(//form/div[@class=\"form-group\"][7]//input)[{}]",
+    "University": "(//form/div[@class=\"form-group\"][7]/following-sibling::div/div[1]//input)[1]",
+    "Fulltime": "(//form/div[@class=\"form-group\"][7]/following-sibling::div/div[1]//select)[1]/option[{}]",
+    "DiplomaCode": "(//form/div[@class=\"form-group\"][7]/following-sibling::div/div[2]//input)[1]",
+    "FromYear": "(//form/div[@class=\"form-group\"][7]/following-sibling::div/div[2]//input)[2]",
+    "ToYear": "(//form/div[@class=\"form-group\"][7]/following-sibling::div/div[2]//input)[3]",
+    "NumOfYear": "(//form/div[@class=\"form-group\"][7]/following-sibling::div/div[2]//input)[4]",
+    "GraduateYear": "(//form/div[@class=\"form-group\"][7]/following-sibling::div/div[2]//input)[5]",
+    "ContactPerson": "(//form/div[@class=\"form-group\"][8]//input)[1]",
+    "ContactPersonPhone": "(//form/div[@class=\"form-group\"][8]//input)[2]",
+    "ContactPersonAddress": "(//form/div[@class=\"form-group\"][9]//input)[1]",
+    "CheckBtn": "//*[@id=\"WNextForm\"]"
+}
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Choose batch.')
+    parser.add_argument('--drivers', type=int, help='Number of drivers', default=2, required=True)
+    parser.add_argument('--wait', type=int, help='Wait to load page (sec)', default=2, required=True)
+    parser.add_argument('--browser', type=str, help='Browser type', default='chrome', required=True)
+    parser.add_argument('--link', type=str, help='Link to regist form', required=True)
+    args = parser.parse_args()
+
+    NUM_DRIVERS = args.drivers
+    BROWSER = args.browser.lower().strip()
+    LINK_FORM = args.link.strip()
+
+    WAIT_TIME = args.wait
+
+    try:
+
+        def fill_form_one_driver(driver, driver_name):
+
+            src_xsl = pd.ExcelFile('./Data.xlsx')
+            df = src_xsl.parse(0)
+            df = df.iloc[:,:-1]
+            df.columns = ['k', 'v']
+            df['v'] = df['v'].fillna('').apply(lambda x: str(x).strip())
+            data = df.set_index('k').to_dict()['v']
+
+            log("[%s] Start fill form" % (driver_name))
+
+            # Row 1
+            fill_text_xpath(driver_name, driver, xpath_config['FullName'], data['FullName'])
+            fill_text_xpath(driver_name, driver, xpath_config['DOB'], data['DOB'])
+            # choose_option_click(driver_name, driver, "/html/body")
+
+            choose_option_click(driver_name, driver, xpath_config['Sex'].format(data['Sex']))
+
+            # Row 2
+            fill_text_xpath(driver_name, driver, xpath_config['Email'], data['Email'])
+            fill_text_xpath(driver_name, driver, xpath_config['Phone'], data['Phone'])
+            choose_option_click(driver_name, driver, xpath_config['Marriage'].format(data['Marriage']))
+
+            # Row 3
+            fill_text_xpath(driver_name, driver, xpath_config['Address'], data['Address'])
+
+            # Row 4
+            choose_option_click(driver_name, driver, xpath_config['Province'].format(data['Province']))
+            time.sleep(1.5)
+            choose_option_click(driver_name, driver, xpath_config['District'].format(data['District']))
+            fill_text_xpath(driver_name, driver, xpath_config['Town'], data['Town'])
+
+            # Row 5
+            fill_text_xpath(driver_name, driver, xpath_config['CurrentAddress'], data['CurrentAddress'])
+
+            # Row 6
+            fill_text_xpath(driver_name, driver, xpath_config['PassportNum'], data['PassportNum'])
+            fill_text_xpath(driver_name, driver, xpath_config['PassportPlace'], data['PassportPlace'])
+            fill_text_xpath(driver_name, driver, xpath_config['PassportFromDate'], data['PassportFromDate'])
+            fill_text_xpath(driver_name, driver, xpath_config['PassportToDate'], data['PassportToDate'])
+
+            # Row 7
+            choose_option_click(driver_name, driver, xpath_config['IsGraduate'].format(data['IsGraduate']))
+            # time.sleep(0.5)
+
+            # Row 7.1
+            fill_text_xpath(driver_name, driver, xpath_config['University'], data['University'])
+            choose_option_click(driver_name, driver, xpath_config['Fulltime'].format(data['Fulltime']))
+
+            # Row 7.2
+            fill_text_xpath(driver_name, driver, xpath_config['DiplomaCode'], data['DiplomaCode'])
+            fill_text_xpath(driver_name, driver, xpath_config['FromYear'], data['FromYear'])
+            fill_text_xpath(driver_name, driver, xpath_config['ToYear'], data['ToYear'])
+            fill_text_xpath(driver_name, driver, xpath_config['NumOfYear'], data['NumOfYear'])
+            fill_text_xpath(driver_name, driver, xpath_config['GraduateYear'], data['GraduateYear'])
+
+            # Row 8
+            fill_text_xpath(driver_name, driver, xpath_config['ContactPerson'], data['ContactPerson'])
+            fill_text_xpath(driver_name, driver, xpath_config['ContactPersonPhone'], data['ContactPersonPhone'])
+
+            # Row 9
+            fill_text_xpath(driver_name, driver, xpath_config['ContactPersonAddress'], data['ContactPersonAddress'])
+
+            # choose_option_click(driver_name, driver, xpath_config['CheckBtn'])
+
+            log("[%s] Finish fill form" % (driver_name))
+
+            #timestamp_now = dt.datetime.today().strftime('%Y-%m-%d_%H-%M-%S')
+            #screenshot_filename = r"./form_screenshot_%s_%s.png" % (driver_name, timestamp_now)
+            #save_screenshot(driver, screenshot_filename)
+
+            #timestamp_now = dt.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+            #print("[%s][%s] Save form screenshot: %s" % (timestamp_now, driver_name, screenshot_filename))
+
+        def run_driver(dn, driver, link_form):
+            try:
+                log("[%s] Start driver" % (dn))
+                driver.maximize_window()
+                driver.get(link_form)
+
+                # wait = ui.WebDriverWait(driver, 5)
+                # wait.until(page_is_loaded)
+
+                log("[%s] Load page completed" % (dn))
+
+                # time.sleep(WAIT_TIME)
+                fill_form_one_driver(driver, dn)
+
+                # driver.close()
+                log("[%s] Close driver" % (dn))
+            except Exception as e:
+                log("[ERROR][%s] %s" % (dn, str(e)))
+
+        pool = ThreadPool(NUM_DRIVERS)
+
+        if BROWSER == 'chrome':
+            from selenium.webdriver.chrome.options import Options
+            caps = DesiredCapabilities().CHROME
+            caps["pageLoadStrategy"] = "normal"
+        elif BROWSER == 'firefox':
+            from selenium.webdriver.firefox.options import Options
+            caps = DesiredCapabilities().FIREFOX
+            caps["pageLoadStrategy"] = "none"
+        else:
+            log("[ERROR] No support browser: %s" % (LINK_FORM))
+        
+        options = Options()
+        options.add_argument("window-size=1920,1080")
+
+        driver_names = ['driver_' + str(i) for i in range(1, NUM_DRIVERS + 1)]                 
+        driver_tuples = pool.map(lambda x: (x, webdriver.Chrome(desired_capabilities=caps, chrome_options=options)), driver_names)
+        
+        res = pool.map(lambda x: run_driver(x[0], x[1], LINK_FORM), driver_tuples)
+    
+    except Exception as e:
+        print(e)
